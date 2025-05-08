432e15839b50afa615dd87f34c8c582cdf277324
UrbanNarrator <urbannarrator5@gmail.com>
2025-05-08 04:10:48 +0300

    Working model

--- App.tsx
+++ App.tsx
@@ -5,7 +5,10 @@ import { AuthProvider } from './context/authContext';
 import { CartProvider } from './context/cartContext';
 import RootNavigator from './navigation/rootNavigator';
 
-
+// Removed invalid JSON-like object
+if ((global as any).HermesInternal) {
+  console.log('Hermes is enabled!');
+}
 
 import './firebaseConfig';
 export default function App() {
